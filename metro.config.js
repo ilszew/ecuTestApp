ad9301e50a1cb87b7234b62ddd4e261f008840ca
Kornel Lewandowski <280101@student.pwr.edu.pl>
2025-11-19 16:46:46 +0100

    Make metro.config.js robust to handle missing local euc-ble-connector package

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,40 @@
 const path = require('path');
+const fs = require('fs');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-
-const root = path.resolve(__dirname, '..', 'euc-ble-connector');
-const pak = require('../euc-ble-connector/package.json');
-
-const modules = Object.keys({
-  ...pak.peerDependencies,
-});
 
 // Helper function to escape regex special characters
 const escape = (str) => {
   return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 };
+
+// Try to find the euc-ble-connector package
+let root = null;
+let modules = [];
+
+try {
+  // First, try the local development path
+  const localPath = path.resolve(__dirname, '..', 'euc-ble-connector', 'package.json');
+  if (fs.existsSync(localPath)) {
+    root = path.resolve(__dirname, '..', 'euc-ble-connector');
+    const pak = require(localPath);
+    modules = Object.keys({
+      ...pak.peerDependencies,
+    });
+  } else {
+    // Try to find it in node_modules (if installed via npm)
+    const nodeModulesPath = path.resolve(__dirname, 'node_modules', 'euc-ble-connector', 'package.json');
+    if (fs.existsSync(nodeModulesPath)) {
+      root = path.resolve(__dirname, 'node_modules', 'euc-ble-connector');
+      const pak = require(nodeModulesPath);
+      modules = Object.keys({
+        ...pak.peerDependencies,
+      });
+    }
+  }
+} catch (error) {
+  console.warn('Warning: Could not find euc-ble-connector package. Metro will use default configuration.');
+  console.warn('Error:', error.message);
+}
 
 /**
  * Metro configuration
@@ -20,19 +43,6 @@ const escape = (str) => {
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-  watchFolders: [root],
-
-  resolver: {
-    blacklistRE: new RegExp(
-      `^${escape(path.join(root, 'node_modules'))}\\/.*$`
-    ),
-
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
-    }, {}),
-  },
-
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -42,6 +52,21 @@ const config = {
     }),
   },
 };
+
+// Only add watchFolders and resolver config if we found the local package
+if (root && fs.existsSync(root)) {
+  config.watchFolders = [root];
+  
+  config.resolver = {
+    blacklistRE: new RegExp(
+      `^${escape(path.join(root, 'node_modules'))}\\/.*$`
+    ),
+    extraNodeModules: modules.reduce((acc, name) => {
+      acc[name] = path.join(__dirname, 'node_modules', name);
+      return acc;
+    }, {}),
+  };
+}
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
 
